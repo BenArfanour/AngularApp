c22599d2ff8a2cca313df73f8521f9b52341735d
nour <benarfanour9@gamil.com>
2018-09-17 17:14:02 +0100

    to the git2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms' ;
 import { Aboutservice } from "../services/about.service";
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
 import { ContactsComponent } from './contacts/contacts.component';
+
+
+
+const appRoutes: Routes = [
+
+{ path: 'about' , component : AboutComponent },
+{ path: 'contacts' , component : ContactsComponent },
+{ path:'**' ,redirectTo: '/about', patchMatch : 'full' }
+
+
+];
 
 @NgModule({
   declarations: [
@@ -14,7 +26,7 @@ import { ContactsComponent } from './contacts/contacts.component';
     ContactsComponent
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule,FormsModule,RouterModule.forRoot(appRoutes)
   ],
   providers: [ Aboutservice ],
   bootstrap: [AppComponent]
