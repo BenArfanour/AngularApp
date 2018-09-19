c45ba9593b8407dbf3d632ed3e20a4a77be963eb
nour <benarfanour9@gamil.com>
2018-09-19 09:07:26 +0100

    final

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -18,7 +18,11 @@ export class GalleryComponent implements OnInit {
 
    pagePhotos:any;
 
-   tab:Array<>:any
+
+   currentPage:number=1 ;
+   size:number=12;
+   pages:Array<number>=[];
+   totalPages:number;
    constructor(private http: HttpClient) { }
 
   ngOnInit() {
@@ -32,9 +36,29 @@ export class GalleryComponent implements OnInit {
 
   	console.log(dataForm);
 
-   this.http.get("https://pixabay.com/api/?key=10132616-1c715792fc727cc043f8fcff8&q="+dataForm.motCle+"&per_page=10&page=1")
-      .subscribe(result => {
-      this.pagePhotos = result.hits ;
-      console.log(this.pagePhotos);
-    })
+       this.http.get("https://pixabay.com/api/?key=10132616-1c715792fc727cc043f8fcff8&q="+dataForm.motCle+"&per_page="+this.size+"&page="+this.currentPage)
+       .subscribe(result => {
+       this.pagePhotos = result ;
+
+
+         
+       this.totalPages = result.totalHits/this.size ;
+
+       console.log(this.totalPages);
+
+       if (result.totalHits % this.size != 0) {++this.totalPages ;}
+       this.pages=new Array(this.totalPages) ;
+
+       console.log(this.pages);
+
+       })
+  
+    }
+
+
+
+
+
+
+
 }
